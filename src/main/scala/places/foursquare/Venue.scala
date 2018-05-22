106c35c548b20172157791886b621dde6a13bc97
Greg Pevnev <gpevnev@gmail.com>
2018-05-22 15:23:09 +0300

    added more categories

--- src/main/scala/places/foursquare/Venue.scala
+++ src/main/scala/places/foursquare/Venue.scala
@@ -11,12 +11,12 @@ case class Location(lat: BigDecimal, lng: BigDecimal, distance: Int, formattedAd
   override def toString: String =
     (s"Расстояние: $distance" +: formattedAddress).mkString("\n")
 }
-case class Category(name: String, id: String) {
-  override def toString: String = name
+case class Category(id: String) {
+  override def toString: String = Categories.Translatioins.russian(this)
 }
 
 trait VenueJsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
-  private implicit val categoryFormat = jsonFormat2(Category)
+  private implicit val categoryFormat = jsonFormat1(Category)
   private implicit val locationFormat = jsonFormat4(Location)
   implicit val venueFormat = jsonFormat3(Venue)
 }
